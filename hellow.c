6d13d553eb435c3c96d684e69a53189da677f6d7
bishalsingh2225 <61987973+bishalsingh2225@users.noreply.github.com>
2020-08-27 20:27:40 +0530

    state of well being
    added a line

--- hellow.c
+++ hellow.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 int main() {
+   printf('i am very happy');
    printf("Hello 1, World!");
    printf("my name is bishal kumar singh");
    printf("i forked this repo and now will send you a pull req");
