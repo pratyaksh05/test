134111af9a2af69163545f2c2a9f34b7febb3c69
bishalsingh2225 <61987973+bishalsingh2225@users.noreply.github.com>
2020-08-27 19:49:41 +0530

    changes made by bishal
    added 2 lines

--- hellow.c
+++ hellow.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 int main() {
    printf("Hello 1, World!");
+   printf("my name is bishal kumar singh");
+   printf("i forked this repo and now will send you a pull req");
    return 0;
 }
