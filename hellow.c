50b5d0ebd08eac489e78b3306924fc16c4e233a5
Pratyaksh Chauhan <55252995+pratyaksh05@users.noreply.github.com>
2020-08-27 20:13:31 +0530

    Update hellow.c

--- hellow.c
+++ hellow.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main() {
    printf("Hello 1, World!");
+   printf("my name is bishal_kumar_singh comit_4");
    printf("i forked this repo and now will send you a pull req");
    printf("pratyaksh");
    return 0;
