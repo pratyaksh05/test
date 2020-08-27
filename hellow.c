35aef0ebbdd90d7a5e59ddc9461d8732577f46f1
bishalsingh2225 <61987973+bishalsingh2225@users.noreply.github.com>
2020-08-27 20:12:54 +0530

    removed line
    my name

--- hellow.c
+++ hellow.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 int main() {
    printf("Hello 1, World!");
-   printf("my name is bishal kumar singh");
    printf("i forked this repo and now will send you a pull req");
    printf("pratyaksh");
    return 0;
